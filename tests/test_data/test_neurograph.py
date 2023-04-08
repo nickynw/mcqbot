ce1a966b96508ccd4b0a428dcdae9750784859a6
Nick Wilson <nicknwilson@outlook.com>
2023-04-08 02:14:35 +0100

    added test

---
+++ tests/test_data/test_neurograph.py
@@ -0,0 +1,8 @@
+"""Test the creation of the sample graph is working as intended"""
+from app.data.neurograph import create_graph, data
+
+def test_graph():
+    """Test the creation of the sample graph is working as intended"""
+    graph = create_graph()
+    assert len(graph.nodes())==len(data)
+    print(graph.edges())
