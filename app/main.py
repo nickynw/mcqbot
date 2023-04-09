45a61a36bfc548688fe92eb8500b511a45b1ac6f
Nick Wilson <nicknwilson@outlook.com>
2023-04-09 01:54:22 +0100

    added mcq generation at root and a live test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 """A basic bare main file for an api using fastapi"""
 from fastapi import FastAPI
+from app.utils.mcq_generator import MCQGenerator
+from app.data.neurograph import create_graph
 
 app = FastAPI()
+
+
+@app.get('/live')
+async def live() -> dict:
+    """Basic test root for fastapi
+
+    Returns:
+        dict: json response
+    """
+    return {}
 
 
 @app.get('/')
@@ -11,4 +23,7 @@ async def root() -> dict:
     Returns:
         dict: json response
     """
-    return {'message': 'Hello World'}
+    graph = create_graph()
+    mcq = MCQGenerator(graph)
+    output = mcq.generate()
+    return output
---
+++ tests/test_main.py
@@ -0,0 +1,23 @@
+"""Test the basic fastapi template"""
+
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_live():
+    """Checks the api was able to run using a basic endpoint"""
+    response = client.get('/live')
+    assert response.status_code == 200
+    assert response.json() == {}
+
+
+def test_root():
+    """Checks the api was able to generate an mcq"""
+    response = client.get('/live')
+    assert response.status_code == 200
+    mcq = response.json()
+    assert 'answer' in mcq
+    assert 'topic' in mcq
+    assert 'choices' in mcq and len(mcq['choices']) > 2
