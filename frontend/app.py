3f571a2aebeab38c2bae7208f6df2210bc07d41e
Nick Wilson <nicknwilson@outlook.com>
2023-06-03 01:22:56 +0100

    changes to linting, new init files, formatting

--- frontend/app.py
+++ frontend/app.py
@@ -1,22 +1,28 @@
-from flask import Flask, render_template, jsonify
+"Basic frontend to serve the Graph Data into an MCQ format"
 import requests
-import random
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home() -> Response:
+    """
+    Renders and displays the homepage.
+
+    Returns:
+        flask.Response: A Flask HTML template
+    """
     try:
-        response = requests.get("http://127.0.0.1:8000/")
-    except Exception:
-        return render_template('error.html',)
-    if response.status_code == 200:
-        try:
+        response = requests.get('http://127.0.0.1:8000/', timeout=1)
+        if response.status_code == 200:
             data = response.json()
-            return render_template('home.html',  **data)
-        except:
-            return render_template('error.html',)
-    return render_template('error.html',)
+            return render_template('home.html', **data)
+    except Exception:
+        pass
+    return render_template(
+            'error.html',
+    )
+
 
 app.run(host='127.0.0.1', port=5000)
