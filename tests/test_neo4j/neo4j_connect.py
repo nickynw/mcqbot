e07c337dffb174ab5c734c996e37760d254069ee
Nick Wilson <nicknwilson@outlook.com>
2023-06-03 03:17:09 +0100

    change neo4j connections order

--- tests/test_neo4j/neo4j_connect.py
+++ tests/test_neo4j/neo4j_connect.py
@@ -8,28 +8,21 @@ load_dotenv()
 
 def try_connections():
     """Attempts to find a working URI as the working URI changes between testing environments."""
-    uri = None
-    try:
-        uri = 'bolt://localhost:7687'
-        Neo4JGraph(uri, 'neo4j', 'password')
-        return uri
-    except Exception:
-        print('%s connection failed. Attempting different URI...' % uri)
-    try:
-        uri = 'bolt://127.0.0.1:7687'
-        Neo4JGraph(uri, 'neo4j', 'password')
-        return uri
-    except Exception:
-        print('%s connection failed. Attempting different URI...' % uri)
-    try:
-        uri = 'bolt://neo4j:7687'
-        Neo4JGraph(uri, 'neo4j', 'password')
-        return uri
-    except Exception:
-        print('%s connection failed. Attempting different URI...' % uri)
-    if uri is None:
+    output = None
+    for uri in [
+        'bolt://neo4j:7687',
+        'bolt://localhost:7687',
+        'bolt://127.0.0.1:7687',
+
+    ]:
+        try:
+            Neo4JGraph(uri, 'neo4j', 'password')
+            output = uri
+        except Exception:
+            print('%s connection failed. Attempting different URI...' % uri)
+    if output is None:
         raise Exception('All uri connection attempts failed.')
-    return uri
+    return output
 
 
 URI = try_connections()
