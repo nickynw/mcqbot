bb3266a0a990a20fbf9d9b117ca212c969b59731
Nick Wilson <nicknwilson@outlook.com>
2023-06-10 02:33:18 +0100

    linting and formatting

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 """A basic bare main file for an api using fastapi"""
 # pylint: disable=unused-argument
-from fastapi.middleware.cors import CORSMiddleware
 from app.core.mcq_generator import MCQGenerator
 from app.data.sample_graph import generate_graph
 from app.models import MCQ
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
@@ -15,17 +15,19 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 origins = [
-    "https://main.d1vo05ddg5t68j.amplifyapp.com/",
-    "http://localhost:8000",
+    'https://main.d1vo05ddg5t68j.amplifyapp.com',
+    'http://localhost:8000',
+    'https://localhost:8000',
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
+
 
 @app.get('/live')
 async def live(request: Request) -> dict:
@@ -38,7 +40,7 @@ async def live(request: Request) -> dict:
     return {}
 
 
-@app.get('/', responses={200: {"model": MCQ}})
+@app.get('/', responses={200: {'model': MCQ}})
 @limiter.limit('5/second')
 async def root(request: Request) -> MCQ:
     """
